f381ed6e1877080fa1b263ce534308bcc746fc55
Chance <chancesmithb@gmail.com>
2024-04-20 14:15:32 -0500

    🐛 FIX: add car object

--- index.ts
+++ index.ts
@@ -1,14 +1,19 @@
-// this orginial code comes from Christian Clausen of Five Lines of Code
+// This orginial code comes from Christian Clausen of Five Lines of Code
 
 enum TrafficLight {
-  RED, YELLOW, GREEN
+  RED,
+  YELLOW,
+  GREEN,
 }
+
+const car = {
+  drive: () => console.log("Car is driving"),
+  stop: () => console.log("Car is stopped"),
+};
 
 const CYCLE = [TrafficLight.RED, TrafficLight.GREEN, TrafficLight.YELLOW];
 
 function updateCarForLight(current: TrafficLight) {
-  if (current === TrafficLight.RED)
-    car.stop();
-  else
-    car.drive();
+  if (current === TrafficLight.RED) car.stop();
+  else car.drive();
 }
